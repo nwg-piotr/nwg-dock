f23d049a453fa20f5bad7ff82442cf4f512ae1ab
piotr <nwg.piotr@gmail.com>
2021-03-19 02:51:02 +0100

    untabify

--- main.go
+++ main.go
@@ -1,65 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"log"
+    "fmt"
+    "log"
 
-	"github.com/dlasky/gotk3-layershell/layershell"
-	"github.com/gotk3/gotk3/gtk"
+    "github.com/dlasky/gotk3-layershell/layershell"
+    "github.com/gotk3/gotk3/gtk"
 )
 
 func main() {
-	tasks, err := listTasks()
-	if err != nil {
-		log.Fatal("Couldn't list tasks:", err)
-	}
+    tasks, err := listTasks()
+    if err != nil {
+        log.Fatal("Couldn't list tasks:", err)
+    }
 
-	for _, task := range tasks {
-		fmt.Printf("%s on WS %v, PID %v, Name: '%s'\n", task.ID, task.WsNum, task.PID, task.Name)
-	}
+    for _, task := range tasks {
+        fmt.Printf("%s on WS %v, PID %v, Name: '%s'\n", task.ID, task.WsNum, task.PID, task.Name)
+    }
 
-	// Initialize GTK without parsing any command line arguments.
-	gtk.Init(nil)
+    // Initialize GTK without parsing any command line arguments.
+    gtk.Init(nil)
 
-	// Create a new toplevel window, set its title, and connect it to the
-	// "destroy" signal to exit the GTK main loop when it is destroyed.
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-	layershell.InitForWindow(win)
+    // Create a new toplevel window, set its title, and connect it to the
+    // "destroy" signal to exit the GTK main loop when it is destroyed.
+    win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+    if err != nil {
+        log.Fatal("Unable to create window:", err)
+    }
+    layershell.InitForWindow(win)
 
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_LEFT, false)
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_BOTTOM, true)
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_RIGHT, false)
+    layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_LEFT, false)
+    layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_BOTTOM, true)
+    layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_RIGHT, false)
 
-	layershell.SetLayer(win, layershell.LAYER_SHELL_LAYER_TOP)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_TOP, 0)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_LEFT, 0)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_RIGHT, 0)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_BOTTOM, 6)
+    layershell.SetLayer(win, layershell.LAYER_SHELL_LAYER_TOP)
+    layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_TOP, 0)
+    layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_LEFT, 0)
+    layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_RIGHT, 0)
+    layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_BOTTOM, 6)
 
-	win.SetTitle("Simple Example")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
+    win.SetTitle("Simple Example")
+    win.Connect("destroy", func() {
+        gtk.MainQuit()
+    })
 
-	// Create a new label widget to show in the window.
-	l, err := gtk.LabelNew("Hello, gotk3!")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
+    // Create a new label widget to show in the window.
+    l, err := gtk.LabelNew("Hello, gotk3!")
+    if err != nil {
+        log.Fatal("Unable to create label:", err)
+    }
 
-	// Add the label to the window.
-	win.Add(l)
+    // Add the label to the window.
+    win.Add(l)
 
-	// Set the default window size.
-	win.SetDefaultSize(800, 30)
+    // Set the default window size.
+    win.SetDefaultSize(800, 30)
 
-	// Recursively show all widgets contained in this window.
-	win.ShowAll()
+    // Recursively show all widgets contained in this window.
+    win.ShowAll()
 
-	// Begin executing the GTK main loop.  This blocks until
-	// gtk.MainQuit() is run.
-	gtk.Main()
+    // Begin executing the GTK main loop.  This blocks until
+    // gtk.MainQuit() is run.
+    gtk.Main()
 }
